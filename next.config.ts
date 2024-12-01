78140f231f0faa845efbe326af9d892bd74e2336
NgollrRomaric <ngolleromaric27@gmail.com>
2024-12-01 21:49:15 +0100

    add to differents files of the project, i am on the part 11 now

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
+// Enable experimental Performance Related Rules (PPR)
 
 export default nextConfig;
